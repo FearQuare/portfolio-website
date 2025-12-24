ef3882a1d52bea5d2b5fd87ed30b6aa34c9e3dec
FearQuare <elmira.aras@protonmail.com>
2025-12-24 22:47:06 +0300

    nav location chnage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const scrollToSection = (id) => {
 }
 
 export default function Navbar() {
-    return(
+    return (
         <nav className="z-50 sticky top-0 flex justify-between content-center backdrop-blur-3xl rounded-b-md">
             <div className="m-2">
                 <img src="fear-light.svg" alt="" className="max-h-10" />
@@ -16,8 +16,8 @@ export default function Navbar() {
             <div className="m-2">
                 <Button variant="text" color="geekblue" onClick={() => scrollToSection('about')}>About</Button>
                 <Button variant="text" color="geekblue" onClick={() => scrollToSection('education')}>Education</Button>
-                <Button variant="text" color="geekblue" onClick={() => scrollToSection('work')}>Work Experience</Button>
                 <Button variant="text" color="geekblue" onClick={() => scrollToSection('volunteering')}>Volunteering</Button>
+                <Button variant="text" color="geekblue" onClick={() => scrollToSection('work')}>Work Experience</Button>
                 <Button variant="text" color="geekblue" onClick={() => scrollToSection('skills')}>Skills</Button>
                 <Button variant="text" color="geekblue" onClick={() => scrollToSection('certificates')}>Certificates</Button>
             </div>
