73b184a8be42720f6b149e4d353b4e52e0b98040
FearQuare <elmira.aras@protonmail.com>
2025-12-24 22:58:11 +0300

    scroll track animation added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { Button } from "antd"
+import { motion, useScroll } from "motion/react"
 
 const scrollToSection = (id) => {
     const el = document.getElementById(id)
@@ -8,19 +9,36 @@ const scrollToSection = (id) => {
 }
 
 export default function Navbar() {
+    const { scrollYProgress } = useScroll()
     return (
-        <nav className="z-50 sticky top-0 flex justify-between content-center backdrop-blur-3xl rounded-b-md">
-            <div className="m-2">
-                <img src="fear-light.svg" alt="" className="max-h-10" />
-            </div>
-            <div className="m-2">
-                <Button variant="text" color="geekblue" onClick={() => scrollToSection('about')}>About</Button>
-                <Button variant="text" color="geekblue" onClick={() => scrollToSection('education')}>Education</Button>
-                <Button variant="text" color="geekblue" onClick={() => scrollToSection('volunteering')}>Volunteering</Button>
-                <Button variant="text" color="geekblue" onClick={() => scrollToSection('work')}>Work Experience</Button>
-                <Button variant="text" color="geekblue" onClick={() => scrollToSection('skills')}>Skills</Button>
-                <Button variant="text" color="geekblue" onClick={() => scrollToSection('certificates')}>Certificates</Button>
-            </div>
-        </nav>
+        <>
+            <motion.div
+                id="scroll-indicator"
+                style={{
+                    scaleX: scrollYProgress,
+                    position: "fixed",
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    height: 10,
+                    originX: 0,
+                    backgroundColor: "oklch(54.6% 0.245 262.881)",
+                }}
+                className="z-40"
+            />
+            <nav className="z-30 sticky pt-2 top-0 flex justify-between content-center backdrop-blur-3xl rounded-b-md">
+                <div className="m-2">
+                    <img src="fear-light.svg" alt="" className="max-h-10" />
+                </div>
+                <div className="m-2">
+                    <Button variant="text" color="geekblue" onClick={() => scrollToSection('about')}>About</Button>
+                    <Button variant="text" color="geekblue" onClick={() => scrollToSection('education')}>Education</Button>
+                    <Button variant="text" color="geekblue" onClick={() => scrollToSection('volunteering')}>Volunteering</Button>
+                    <Button variant="text" color="geekblue" onClick={() => scrollToSection('work')}>Work Experience</Button>
+                    <Button variant="text" color="geekblue" onClick={() => scrollToSection('skills')}>Skills</Button>
+                    <Button variant="text" color="geekblue" onClick={() => scrollToSection('certificates')}>Certificates</Button>
+                </div>
+            </nav>
+        </>
     )
 }
