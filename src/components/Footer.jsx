84ecdb9f69b32f74e146a68628d20832269f4aea
FearQuare <elmira.aras@protonmail.com>
2025-12-27 14:54:25 +0300

    footer added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import { HeartFilled } from '@ant-design/icons';
+
+export default function Footer() {
+    return (
+        <div className="w-full bg-indigo-700/70 rounded-t-md h-10 flex justify-center items-center text-white">
+            <p>Made with <HeartFilled /> FearQuare</p>
+        </div>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Navbar from './components/Navbar.jsx'
+import Footer from './components/Footer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -10,5 +11,6 @@ createRoot(document.getElementById('root')).render(
       <Navbar/>
       <App />
     </div>
+    <Footer />
   </StrictMode>
 )
