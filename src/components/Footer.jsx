bc1ce35cae10ef355bd3e6164afb8fedf841c03c
FearQuare <elmira.aras@protonmail.com>
2025-12-28 16:51:34 +0300

    progress cards responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { HeartFilled } from '@ant-design/icons';
 export default function Footer() {
     return (
         <div className="w-full bg-indigo-700/70 rounded-t-md h-10 flex justify-center items-center text-white">
-            <p>Made with <HeartFilled /> FearQuare</p>
+            <p>Made with <HeartFilled /> by FearQuare</p>
         </div>
     )
 }
--- src/components/ProgressCard.jsx
+++ src/components/ProgressCard.jsx
@@ -1,7 +1,7 @@
 import { Card, Progress } from "antd"
 export default function ProgressCard({skillName, progressStatus}){
     return (
-        <Card className="min-w-64 text-center" title={skillName}>
+        <Card className="min-w-32 lg:min-w-64 text-center" title={skillName}>
             <Progress type="circle" percent={progressStatus}/>
         </Card>
     )
